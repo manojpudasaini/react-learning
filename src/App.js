a1e41c5f7185b92a446b07716a722b1580e3d0cf
manojpudasaini <mpudasaini17@gmail.com>
2020-07-10 20:13:12 +0545

    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>
+    <h1>Hello</h1>
+    <h1>I'm learning ReactJS</h1>
+    </p>
+  );
+}
+
+export default App;
