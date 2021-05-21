56e3d99968bbd4e344973c877f5a20e70ddd72e3
manojpudasaini <mpudasaini17@gmail.com>
2021-05-21 17:18:17 +0545

    final touches

--- src/components/covid/Covid.js
+++ src/components/covid/Covid.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react'
-import {AppBar, Typography, Container, Avatar, FormHelperText } from "@material-ui/core";
+import {AppBar, Typography, Container, Avatar } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
