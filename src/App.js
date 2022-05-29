c687ac22f1f59026327a62cf8499792cafe250b7
manojpudasaini <mpudasaini17@gmail.com>
2022-05-29 13:11:26 +0545

    weather app built

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BsSearch } from "react-icons/bs";
+import { useState } from "react";
 function App() {
+  const api = {
+    base: "https://api.openweathermap.org/data/2.5/",
+    key: "ee34b2e317f61b897abed3816bea631e",
+  };
+  const [weather, setWeather] = useState({});
+  const [location, setLocation] = useState("");
+  const fetchApi = () => {
+    fetch(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => setWeather(result))
+      .then(setLocation(""));
+  };
+  console.log(weather);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="input-container"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button className="button-container" onClick={fetchApi}>
+          <BsSearch className="search-icon" />
+        </button>
+      </div>
+      <div className="data-container">
+        <p>{weather?.main?.temp}</p>
+        <p>{weather?.name}</p>
+      </div>
     </div>
   );
 }
